367f66f671b6befa006ca3867fdc676557cd57eb
PabloEsteban87 <pablosebas87@gmail.com>
2023-08-27 18:13:41 +0200

    slider finalizado

--- front/src/app/Events/components/slider/slider.component.ts
+++ front/src/app/Events/components/slider/slider.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent implements OnInit{
-  array_images!: any [5];
-  
-  
+export class SliderComponent implements OnInit {
+  array_images: any = Array();
+  j: any = 0;
+  imagePath!: string;
+
+  constructor(private renderer: Renderer2) {}
+
   ngOnInit(): void {
-    for(let i = 1; i<=this.array_images.lenght; i++){
-    this.array_images = "slider" + i;
-    console.log(this.array_images);
+    for (let i = 1; i <= 5; i++) {
+      this.array_images.push('../../../../assets/slider/slider' + i + '.png');
+    }
 
-
+    this.imagePath = this.array_images[this.j];
   }
-}
 
+  left() {
+    if (this.j <= 0) {
+      this.j = 4;
+      this.imagePath = this.array_images[this.j];
+    } else {
+      this.j--;
+      this.imagePath = this.array_images[this.j];
+    }
+  }
 
-
-
-  
-
-
-
-
+  right() {
+    if (this.j >= 4) {
+      this.j = 0;
+      this.imagePath = this.array_images[this.j];
+    } else {
+      this.j++;
+      this.imagePath = this.array_images[this.j];
+    }
+  }
 }
