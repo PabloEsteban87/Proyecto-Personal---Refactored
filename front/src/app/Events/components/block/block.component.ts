5e7df648a9b58bd3da942a45bba3bd89c7fefaec
PabloEsteban87 <pablosebas87@gmail.com>
2023-09-13 12:25:00 +0200

    guardado

--- front/src/app/Events/components/block/block.component.ts
+++ front/src/app/Events/components/block/block.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-block',
   templateUrl: './block.component.html',
-  styleUrls: ['./block.component.scss']
+  styleUrls: ['./block.component.scss'],
 })
-export class BlockComponent implements OnInit{
+export class BlockComponent implements OnInit {
   allBooks: any[] = [];
   url: string | undefined;
 
- 
 
-  constructor(private booksService: BooksService) {}
+  constructor(
+    private booksService: BooksService,
+    private renderer: Renderer2
+  ) {}
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.loadAllBooks();
     this.loadpath();
   }
@@ -25,10 +27,22 @@ export class BlockComponent implements OnInit{
     });
   }
 
-  loadpath(){
-    this.url = "../../../../assets/libros/"
+  loadpath() {
+    this.url = '../../../../assets/libros/';
   }
 
+  onMouseEnter(hoverName: HTMLElement, back: HTMLElement, front: HTMLElement) {
+    hoverName.style.transform =
+      'perspective(600px) rotateY(-180deg) translateX(100%)';
+    back.style.display = 'block';
+    back.style.position = 'absolute';
+  }
+
+  onMouseOut(hoverName: HTMLElement, back: HTMLElement, front: HTMLElement){
+    hoverName.style.transform =
+    'perspective(600px) rotateY(0deg) translateX(0%)';
+  back.style.display = 'block';
+  }
 
 
 }
