0d6d9b1e183c7940d40b77d9c5518347f5655955
PabloEsteban87 <pablosebas87@gmail.com>
2023-09-20 09:44:01 +0200

    guardado

--- front/src/app/Events/views/viewrecommended/viewrecommended.component.ts
+++ front/src/app/Events/views/viewrecommended/viewrecommended.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecommendedBooksService } from '../../services//Recommended/recommended-books.service';
 
 @Component({
   selector: 'app-viewrecommended',
   templateUrl: './viewrecommended.component.html',
   styleUrls: ['./viewrecommended.component.scss']
 })
-export class ViewrecommendedComponent {
+export class ViewrecommendedComponent implements OnInit {
+
+  allRecommendedBooks: any[] = [];
+
+  constructor(private recommendedBooksService: RecommendedBooksService){}
+
+
+  ngOnInit(): void {
+    this.loadAllRecommended();
+  }
+
+  loadAllRecommended(){
+    this.recommendedBooksService.getRecommendedBooks().subscribe((recommendedbooks: any[]) => {
+      this.allRecommendedBooks = recommendedbooks;
+    }); 
+  }
 
 }
