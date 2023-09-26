fd16f8cdd3ce2d722c16237436f02b0a82c28738
PabloEsteban87 <pablosebas87@gmail.com>
2023-09-26 08:52:42 +0200

    guardado

--- front/src/app/shared/header/header.component.ts
+++ front/src/app/shared/header/header.component.ts
@@ -1,35 +1,32 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
-
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit{
-
-  public active : boolean = true;
+export class HeaderComponent implements OnInit {
+  public active: boolean = true;
   @ViewChild('containernav2mobile') menuMovil!: ElementRef;
 
-  bodyElement! : any; 
+  bodyElement!: any;
 
-  constructor(private renderer: Renderer2){}
+  constructor(private renderer: Renderer2) {}
 
-  showMenu() : void{
-    
-    if(this.active){
-     
-      this.renderer.addClass(this.menuMovil.nativeElement, "show-menu");
-/*        this.renderer.addClass(this.bodyElement, "body-blur"); */
-      this.renderer.removeClass(this.menuMovil.nativeElement, "hide-menu");
-      this.renderer.removeClass(this.menuMovil.nativeElement, 'hideDisplay');  
-
-    }
-    
-    else{
-      this.renderer.removeClass(this.menuMovil.nativeElement, "show-menu");
-/*        this.renderer.removeClass(this.bodyElement, "body-blur");  */
-      this.renderer.addClass(this.menuMovil.nativeElement, "hide-menu");
+  showMenu(): void {
+    if (this.active) {
+      this.renderer.addClass(this.menuMovil.nativeElement, 'show-menu');
+      this.renderer.removeClass(this.menuMovil.nativeElement, 'hide-menu');
+      this.renderer.removeClass(this.menuMovil.nativeElement, 'hideDisplay');
+    } else {
+      this.renderer.removeClass(this.menuMovil.nativeElement, 'show-menu');
+      this.renderer.addClass(this.menuMovil.nativeElement, 'hide-menu');
       setTimeout(() => {
         this.renderer.addClass(this.menuMovil.nativeElement, 'hideDisplay');
       }, 1500);
@@ -38,25 +35,6 @@ export class HeaderComponent implements OnInit{
   }
 
   ngOnInit() {
-this.bodyElement = document.querySelector('body'); 
-}
-
- /*  openMenu(){
-    if (this.isFront) {
-      console.log(this.containernav);
-      this.renderer.removeClass(this.containernav, 'ocultar-menu')
-      this.renderer.addClass(this.containernav, 'show-menu')
-     this.renderer.addClass(this.nav.getElement, 'show-menu');  
+    this.bodyElement = document.querySelector('body');
   }
-  else{
-    this.renderer.addClass(this.containernav, 'ocultar-menu')
-    this.renderer.removeClass(this.containernav, 'show-menu')
-  }
-  this.isFront = !this.isFront;
-  }
- */
-/*   openMenu(){
-    this.nav.getElement();
-  /*   this.renderer.addClass(this.nav.getElement, 'show-menu');  */
-  /* } */
 }
